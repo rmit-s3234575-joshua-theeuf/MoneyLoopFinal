4f21e55ca6791d3f53ec943cacfe76751e9a3a55
deepozzzie <s3234575@student.rmit.edu.au>
2018-08-31 14:27:10 +1000

    update's the routes, customer controller, and companies controller to support apis

---
+++ app/controllers/api/v1/customers_controller.rb
@@ -0,0 +1,24 @@
+class Api::V1::CustomersController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  private
+  def customer_params
+    params.permit(:given_name, :surname, :email, :phone_mobile, :phone_home,:dob, :address, :employer_name, :job_title, :device_type, :device_os, :device_model, :device_screen_resolution, :ip_location, :time_zone, :time_of_day :company_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,25 @@
 Rails.application.routes.draw do
-  resources :customers
   resources :companies
+
   resources :indices
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #define namespace for the customers api
+  # the company does not need to be an api as  a company will be greated by MoneyLoop when a services agreement is signed.
+
+  namespace 'api' do
+    namespace 'v1' do
+      resources :customers
+      get "/customers", to: 'customers#index'
+      get "/customers(.:format)", to: 'customers#index'
+      post "/customers(.:format)", to: 'customers#create'
+      get "/customers/new(.:format)",to: 'customers#new'
+      get "/customers/:id/edit(.:format)", to: 'customers#edit'
+      get "/customers/:id(.:format)", to: 'customers#show'
+      patch "/customers/:id(.:format)", to: 'customers#update'
+      put "/customers/:id(.:format)", to: 'customers#update'
+      delete "/customers/:id(.:format)", to: 'customers#destroy'
+    end
+  end
+
   root "indices#index"
 end
