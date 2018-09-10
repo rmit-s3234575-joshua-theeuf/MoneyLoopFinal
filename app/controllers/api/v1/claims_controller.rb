d865ed3bfe3cbf71760c2242eff2bccafcdf63af
deepozzzie <s3234575@student.rmit.edu.au>
2018-09-10 13:31:39 +1000

    added structure to claims_controller to add new claims via api endpoint.

--- app/controllers/api/v1/claims_controller.rb
+++ app/controllers/api/v1/claims_controller.rb
@@ -1,2 +1,21 @@
 class Api::V1::ClaimsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+  private
+  def claims_params
+      params.permit(:given_name, :surname, :email, :phone_mobile, :phone_home,:dob, :address, :employer_name, :job_title, :device_type, :device_os, :device_model, :device_screen_resolution, :ip_location, :time_zone, :time_of_day, :company_id)
+  end
 end
