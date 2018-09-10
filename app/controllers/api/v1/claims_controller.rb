67a5c19c567f94ae46497a946ee0d295b79344c6
deepozzzie <s3234575@student.rmit.edu.au>
2018-09-10 13:46:42 +1000

    added crud functionality to claims via api

--- app/controllers/api/v1/claims_controller.rb
+++ app/controllers/api/v1/claims_controller.rb
@@ -1,21 +1,57 @@
 class Api::V1::ClaimsController < ApplicationController
   def index
+    claim = Claim.all
+    render json: {status: "Success", message: "Claim Details", data: claim}, status: :ok
   end
 
   def show
+    begin
+      claim = Claim.find(params[:id])
+      render json: {status: "Success", message: "Claim Details", data:claim}, status: :ok
+    rescue ActiveRecord::RecordNotFound => e
+      render json: {status: "Failed", message: "Object not found where id = #{params[:id]}"}, status: :unprocessable_entity
+    end
   end
 
   def create
+    claim  = Claim.new(claim_params)
+    #create a claim.
+    if claim.save
+      render json: {status: "Success", message: "Created", data:claim}, status: :ok
+    else
+      render json: {status: "failed", message: "failed to create object", data:claim.errors}, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    begin
+      claim = Claim.find(params[:id])
+      claim.destroy
+      render json: {status: "Success", message: "Succesfully destroyed"}, status: :ok
+    rescue ActiveRecord::RecordNotFound => e
+      render json: {status: "failed", message: "failed to destroy object where id = #{params[:id]}"}, status: :unprocessable_entity
+    end
   end
 
   def update
+    begin
+      claim = Claim.find(params[:id])
+      if claim.update_attributes(customer_params)
+        render json: {status: "Success", message: "updated", data:claim}, status: :ok
+      else
+        render json: {status: "failed", message: "failed to update", data:claim.errors}, status: :unprocessable_entity
+      end
+    rescue ActiveRecord::RecordNotFound => e
+      render json: {status: "failed", message: "failed to find object where id = #{params[:id]}"}, status: :unprocessable_entity
+    end
   end
 
   private
   def claims_params
-      params.permit(:given_name, :surname, :email, :phone_mobile, :phone_home,:dob, :address, :employer_name, :job_title, :device_type, :device_os, :device_model, :device_screen_resolution, :ip_location, :time_zone, :time_of_day, :company_id)
+      params.permit(:company_id, :customer_id, :exposure, :date_of_origination, :credit_score)
+  end
+
+  #this is where we will intereact with the infrenetics credit model.
+  def claculate_credit_score
   end
 end
