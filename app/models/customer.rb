41588461d88e36e1f043cfff18afa5d20dc126b9
deepozzzie <s3234575@student.rmit.edu.au>
2018-09-10 15:14:26 +1000

    added some validation for customer creation.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,3 @@
 class Customer < ApplicationRecord
+    validates :given_name,:surname, :email,:phone_mobile, :phone_home,:dob, :address, :employer_name, :job_title,:device_os, :device_type, :device_model, :device_screen_resolution, :ip_location, :time_zone :presence => true
 end
