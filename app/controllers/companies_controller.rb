08d85a41d446764b1ea65b23b1a36c9bd047141f
deepozzzie <s3234575@student.rmit.edu.au>
2018-09-10 11:59:03 +1000

    add

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,6 +2,9 @@ class Api::V1::CompaniesController < ApplicationController
   def index
   end
 
+  def create
+  end
+
   def show
   end
 
@@ -9,5 +12,12 @@ class Api::V1::CompaniesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  #name is the company name, abn is the Australian business number, tfn is the taxfile number, type is what type of insurance product do they offer.
+  #i.e. 1. Excess paid to insurer, 2. Co-Payment
+  def company_params
+    params.permit(:name, :abn, :acn, :tfn, :contact_number, :contact_email, :type)
   end
 end
