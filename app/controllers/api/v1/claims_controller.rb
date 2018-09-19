0ce6ffaa5832d69e8a7490cec2acf93e9eaac9ae
Joshua Theeuf <joshua.theeuf@moneyloop.com.au>
2018-09-19 10:49:54 +1000

    change

--- app/controllers/api/v1/claims_controller.rb
+++ app/controllers/api/v1/claims_controller.rb
@@ -80,7 +80,7 @@ class Api::V1::ClaimsController < ApplicationController
       "email" => "#{customer.email}",
       "phone_mobile" => "#{customer.phone_mobile}",
       "phone_home" => "#{customer.phone_home}",
-      "dob" => "#{customer.dob.strftime(%d%m%Y)}",
+      "dob" => "#{customer.dob.to_date}",
       "address" => "#{customer.address}",
       "employer_name" => "#{customer.employer_name}",
       "job_title" => "#{customer.job_title}",
