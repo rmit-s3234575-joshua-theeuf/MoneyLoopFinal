28932c874e8b00bdc8c3fc68260852173d877141
Joshua Theeuf <joshua.theeuf@moneyloop.com.au>
2018-10-09 14:05:14 +1100

    try

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -11,7 +11,7 @@ class Api::V1::CustomersController < ApplicationController
 
   def create
     customer = Customer.new(customer_params)
-
+    byebug
     #create a customer.
     customer.update(dob:customer_params[:dob].to_date.strftime("%d%m%Y"))
     if customer.save
@@ -109,7 +109,6 @@ class Api::V1::CustomersController < ApplicationController
       response.code
       response.body
       $content = {code: JSON.parse(response.code), body: JSON.parse(response.body)}
-      byebug
       if $content[:code] != 200
         return false
       end
