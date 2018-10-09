f12731d4ce1564c851584435ee72dff8adc9f351
Joshua Theeuf <joshua.theeuf@moneyloop.com.au>
2018-10-09 11:54:02 +1100

    calculate_credit_score

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -15,7 +15,7 @@ class Api::V1::CustomersController < ApplicationController
       byebug
       claim = Claim.new(:customer_id => customer.id, :company_id => customer.company_id, :exposure => customer.exposure, :date_of_origination => customer.date_of_origination)
       if claim.save
-        Claim.claculate_credit_score(customer, claim)
+        calculate_credit_score(customer, claim)
         render json: {status: "Success", message: "Created", data:customer}, status: :ok
       else
         render json: {status: "failed", message: "failed to create object", data:customer.errors}, status: :unprocessable_entity
@@ -67,7 +67,51 @@ class Api::V1::CustomersController < ApplicationController
     end
   end
   #this is where we will intereact with the infrenetics credit model.
-  def claculate_credit_score
+  def calculate_credit_score
+    byebug
+    uri = URI.parse("https://api.inferentics.com/v1")
+    request = Net::HTTP::Post.new(uri)
+    request.content_type = "application/json"
+    request["Authorization"] = "Token test_1825b34257c8398b035c110edd03b0911353c0d5155f7ee5d3738467b6"
+    request.body = JSON.dump({
+      "exposure" => claim.exposure,
+      "given_names" => "#{customer.given_names}",
+      "surname" => "#{customer.surname}",
+      "email" => "#{customer.email}",
+      "phone_mobile" => "#{customer.phone_mobile}",
+      "phone_home" => "#{customer.phone_home}",
+      "dob" => "#{customer.dob}",
+      "address" => "#{customer.address}",
+      "employer_name" => "#{customer.employer_name}",
+      "job_title" => "#{customer.job_title}",
+      "device_type" => "#{customer.device_type}",
+      "device_os" => "#{customer.device_os}",
+      "device_model" => "#{customer.device_model}",
+      "device_screen_resolution" => "#{customer.device_screen_resolution}",
+      "network_service_provider" => "#{customer.network_service_provider}",
+      "ip_address" => "#{customer.ip_location}",
+      "time_zone" => "#{customer.time_zone}",
+      "time_of_day" => "#{customer.time_of_day}"
+      })
 
+      req_options = {
+        use_ssl: uri.scheme == "https",
+      }
+      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+        http.request(request)
+      end
+      response.code
+      response.body
+      credit_score = JSON.parse(response.body)
+      #business rules for approval and rejection
+      if customer.update(credit_score: credit_score["result"])
+        if credit_score['result'].to_i >= 750
+          claim.approved = true
+        else
+          claim.approved = false
+        end
+        return
+      end
+    end
   end
 end
