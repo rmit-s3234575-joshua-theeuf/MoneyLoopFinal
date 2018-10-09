648e43ff8eee001ce65ef7e3d1b324be0a6dab4b
Joshua Theeuf <joshua.theeuf@moneyloop.com.au>
2018-10-09 13:28:10 +1100

    this

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -110,9 +110,9 @@ class Api::V1::CustomersController < ApplicationController
       response.code
       response.body
       byebug
-      $content['code'] = JSON.parse(response.code)
-      $content['body'] = JSON.parse(response.body)
-      if content['code'] != 200
+      $content[:code] = JSON.parse(response.code)
+      $content[:body] = JSON.parse(response.body)
+      if content[:code] != 200
         return false
       end
       credit_score = JSON.parse(response.body)
