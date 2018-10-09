634f735d10c97dcc4becc9d13f33f40b37ef244e
Joshua Theeuf <joshua.theeuf@moneyloop.com.au>
2018-10-09 11:45:39 +1100

    try

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -13,7 +13,7 @@ class Api::V1::CustomersController < ApplicationController
     customer.update(dob:customer_params[:dob].to_date.strftime("%d%m%Y"))
     if customer.save
       byebug
-      claim = Claim.create(customer.id, customer_params[:exposure], customer_params[:date_of_origination])
+      Claim.create(:customer_id => customer.id, :company_id => customer.company_id, :exposure => customer.exposure, :date_of_origination => customer.date_of_origination)
       render json: {status: "Success", message: "Created", data:customer}, status: :ok
     else
       render json: {status: "failed", message: "failed to create object", data:customer.errors}, status: :unprocessable_entity
