e6bdb5b2fe8841a84b6b659fdf2465caf57f8373
Joshua Theeuf <joshua.theeuf@moneyloop.com.au>
2018-10-09 13:34:22 +1100

    json error message.

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -23,7 +23,7 @@ class Api::V1::CustomersController < ApplicationController
         byebug
         render json: {status: "Success", message: "Created", data:{"claim": claim, "customer":customer}}, status: :ok
       else
-        render json: {status: "failed", message: "Couldnt create claim", data: response.body}, status: :unprocessable_entity
+        render json: {status: "Failed", message: "Failed to generate claim and customer. See json body for error message. ", data: $content[:body]}, status: :unprocessable_entity
       end
       else
         render json: {status: "failed", message: "failed to create object", data:customer.errors}, status: :unprocessable_entity
